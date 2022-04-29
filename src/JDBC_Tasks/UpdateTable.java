50f19785ed0057ad0944c065e79edac212804c46
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-04-29 10:08:33 +0200

    onlne rev2

--- src/JDBC_Tasks/UpdateTable.java
+++ src/JDBC_Tasks/UpdateTable.java
@@ -7,43 +7,40 @@ import java.sql.SQLException;
 
 public class UpdateTable
 {
-        ConnInfo connInfo =new ConnInfo();
-        private final String url = connInfo.getUrl();
-        private final String user = connInfo.getUser();
-        private final String password = connInfo.getPassword();
+    ConnInfo connInfo =new ConnInfo();
+    private final String url = connInfo.getUrl();
+    private final String user = connInfo.getUser();
+    private final String password = connInfo.getPassword();
 
 
-        public Connection connect() throws SQLException {
-            return DriverManager.getConnection(url, user, password);
-        }
+    public Connection connect() throws SQLException {
+    return DriverManager.getConnection(url, user, password);
+       }
 
-        public int updateAll(String database) {
+    public int updateAll(String database) {
 
-            String SQL = "select * from" + database+ "order by medarbejderID";
+    String SQL = "select * from" + database+ "order by medarbejderID";
 
-            int affectedrows = 0;
+    int affectedrows = 0;
 
-            try (Connection conn = connect();
-                 PreparedStatement pstmt = conn.prepareStatement(SQL)) {
-                 affectedrows = pstmt.executeUpdate();
+    try (Connection conn = connect();
+         PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+         affectedrows = pstmt.executeUpdate();
 
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-            return affectedrows;
+         } catch (SQLException ex) {
+           System.out.println(ex.getMessage());
+         }
+         return affectedrows;
         }
 
         /*
          * @param args the command line arguments
          */
-        public void main(String[] args) {
-            UpdateTable updateTable = new UpdateTable();
-            updateTable.updateAll("medarbejdere");
-            updateTable.updateAll("vagter");
-            updateTable.updateAll("tjekind");
-            updateTable.updateAll("login");
-
-
-
-        }
+    public void main(String[] args) {
+        UpdateTable updateTable = new UpdateTable();
+        updateTable.updateAll("medarbejdere");
+        updateTable.updateAll("vagter");
+        updateTable.updateAll("tjekind");
+        updateTable.updateAll("login");
+    }
 }
